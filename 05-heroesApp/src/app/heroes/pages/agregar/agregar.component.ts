30714d3e95a16b0c06c28b70e53566e14779581b
masset151 <masset.ruiz151@gmail.com@gmail.com>
2021-04-16 09:47:38 +0200

    se implementa la funcionalidad añadir heroe, se repara el pipe imagenes y se controlan las exepciones

--- 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from 'src/app/interfaces/heroes.interface';
 import { HeroesServiceService } from 'src/app/services/heroes-service.service';
 import { Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import {switchMap} from 'rxjs/operators'
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [` 
+      img{
+        width:100%;
+        border-radius:5px;
+      }
+
+  `]
 })
 export class AgregarComponent implements OnInit {
 
@@ -37,19 +44,33 @@ export class AgregarComponent implements OnInit {
 
   };
 
-  constructor(private heroesService:HeroesService) { }
+  constructor(private heroesService:HeroesService,private activateRoute:ActivatedRoute,private router:Router) { }
 
   guardar(){
     if(this.heroe.superhero.trim().length==0){
       return ;
     }
 
-    this.heroesService.agregarHeroe(this.heroe).subscribe(resp =>{
-      console.log('Respuesta',resp)
-    })
+    if(this.heroe.id){
+      this.heroesService.actualizarHeroe(this.heroe).subscribe(heroe => console.log('Actualizando',heroe))
+    }else{
+      this.heroesService.agregarHeroe(this.heroe).subscribe(resp =>{
+        this.router.navigate(['/heroes/editar',resp.id]);
+      })
+    
+    }
+
+    
   }
 
   ngOnInit(): void {
+    
+    if(!this.router.url.includes('editar')){
+      return ;
+    }
+    
+    this.activateRoute.params.pipe(switchMap(({id})=> this.heroesService.getHeroePorId(id)))
+    .subscribe(heroe => this.heroe = heroe)
   }
 
 }
